0aa614bc3b322a813f94c32d85d490d95b27d87c
Bennett Johnson <bennettjohnson0@gmail.com>
2021-12-11 17:15:31 -0600

    fixed things, worked on package.json

--- src/extension.ts
+++ src/extension.ts
@@ -1,63 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as pages from './page';
 
-var openers;
-var closers;
-
-
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Smart Pageupdown activated.');
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let pgup = vscode.commands.registerCommand('spd.pageup', () => {
-		const editor = vscode.window.activeTextEditor;
-		if(editor?.selection !== undefined){
-			const position = editor.selection.active;
-			let selectionPosition = pages.pageUp(editor.document, position);
-			editor.selection = new vscode.Selection(selectionPosition, selectionPosition);
-			editor.revealRange(new vscode.Range(selectionPosition,selectionPosition));
-		}
-	});
-	let pgdn = vscode.commands.registerCommand('spd.pagedown', () => {
-		const editor = vscode.window.activeTextEditor;
-		if(editor?.selection !== undefined){
-			const position = editor.selection.active;
-			let selectionPosition = pages.pageDown(editor.document, position);
-			editor.selection = new vscode.Selection(selectionPosition, selectionPosition);
-			editor.revealRange(new vscode.Range(selectionPosition,selectionPosition));
-		}
-	});
-	let shiftpgdn = vscode.commands.registerCommand('spd.shiftpagedown', () => {
-		const editor = vscode.window.activeTextEditor;
-		if(editor?.selection !== undefined){
-			const position = editor.selection.active;
-			const anchor = editor.selection.anchor;
-			let selectionPosition = pages.pageDown(editor.document, position);
-			editor.selection = new vscode.Selection(anchor, selectionPosition);
-			editor.revealRange(new vscode.Range(selectionPosition,selectionPosition));
-		}
-	});
-	let shiftpgup = vscode.commands.registerCommand('spd.shiftpageup', () => {
-		const editor = vscode.window.activeTextEditor;
-		if(editor?.selection !== undefined){
-			const position = editor.selection.active;
-			const anchor = editor.selection.anchor;
-			let selectionPosition = pages.pageUp(editor.document, position);
-			editor.selection = new vscode.Selection(anchor, selectionPosition);
-			editor.revealRange(new vscode.Range(selectionPosition,selectionPosition));
-		}
-	});
+	// prep config
+	pages.updateConfig();
+	//listen for changes to settings.
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+		pages.updateConfig();
+	}));
 
-	context.subscriptions.push(pgup, pgdn, shiftpgdn,shiftpgup);
+	type SelectFunc = (editor: vscode.TextEditor, position: vscode.Position) => void;
+	type PageFunc = (document: vscode.TextDocument, position: vscode.Position) => vscode.Position;
+
+	const updateSelection: SelectFunc = (editor: vscode.TextEditor, position: vscode.Position) => {
+		editor.revealRange(new vscode.Range(position,position));
+		editor.selection = new vscode.Selection(position, position);
+	};
+	const updateSelectionAnchor: SelectFunc = (editor: vscode.TextEditor, position: vscode.Position) => {
+		editor.revealRange(new vscode.Range(position,position));
+		editor.selection = new vscode.Selection(editor.selection.anchor, position);
+	};
+	const runIfSelection =(editor: vscode.TextEditor, update: SelectFunc, getPosition: PageFunc) => {
+		if(editor.selection){
+			update(editor, getPosition(editor.document, editor.selection.active));
+		}
+	};
+	const COMMANDS = {
+		pageup: (editor: vscode.TextEditor) => runIfSelection(editor, updateSelection, pages.pageUp),
+		pagedown: (editor: vscode.TextEditor) => runIfSelection(editor, updateSelection, pages.pageDown),
+		shiftpageup: (editor : vscode.TextEditor) => runIfSelection(editor, updateSelectionAnchor, pages.pageUp),
+		shiftpagedown: (editor : vscode.TextEditor) => runIfSelection(editor, updateSelectionAnchor, pages.pageDown)
+	};
+
+	for(let [name, func] of Object.entries(COMMANDS)){
+		context.subscriptions.push(
+			vscode.commands.registerTextEditorCommand('spd.' + name, func)
+		);
+	}
 }
 
 // this method is called when your extension is deactivated
--- src/page.ts
+++ src/page.ts
@@ -1,72 +1,71 @@
 import * as vscode from 'vscode';
 
-const CONFIG = vscode.workspace.getConfiguration('Page Up/Down');
-const MAX_LINES = CONFIG.get("maxPageSize") as number;
-const OPENERS: string[] = [];
-const CLOSERS: string[] = [];
-if(CONFIG.get("useCurlyBrackets")){
-    OPENERS.push('{');
-    CLOSERS.push('}');
-}
-if(CONFIG.get("useSquareBrackets")){
-    OPENERS.push('[');
-    CLOSERS.push(']');
-}
-if(CONFIG.get("useParentheses")){
-    OPENERS.push('(');
-    CLOSERS.push(')');
-}
+var MAX_LINES: number;
+var OPENERS: string[];
+var CLOSERS: string[];
 
-
-export function pageDown(doc: vscode.TextDocument, position: vscode.Position): vscode.Position{
-    if(!OPENERS.length){
-        return new vscode.Position(position.line + MAX_LINES, 0);
+export function updateConfig(){
+    const CONFIG = vscode.workspace.getConfiguration('Page Up/Down');
+    OPENERS = [];
+    CLOSERS = [];
+    MAX_LINES = CONFIG.get("maxPageSize") as number;
+    if(CONFIG.get("useCurlyBrackets")){
+        OPENERS.push('{');
+        CLOSERS.push('}');
     }
-
+    if(CONFIG.get("useSquareBrackets")){
+        OPENERS.push('[');
+        CLOSERS.push(']');
+    }
+    if(CONFIG.get("useParentheses")){
+        OPENERS.push('(');
+        CLOSERS.push(')');
+    }
+}
+export function pageDown(doc: vscode.TextDocument, position: vscode.Position): vscode.Position{
     let text = doc.getText(new vscode.Range(position, new vscode.Position(position.line + MAX_LINES, 0)));
-    let lines = text.split(/\r?\n/);
+    let lines: string[] = text.split(/\r?\n/);
+    if(!OPENERS.length){
+        return new vscode.Position(Math.min(position.line + MAX_LINES, position.line + lines.length - 1), 0);
+    }
     let count= 0;
     let bracketType;
+
     for(let line = 0; line < lines.length; line++){
-        let thisLine = lines[line];
-        for(let index = 0; index < thisLine.length; index++){
-            let char = thisLine[index];
+        let currentLine = lines[line];
+        for(let index = 0; index < currentLine.length; index++){
+            let char = currentLine[index];
+
             if(bracketType === undefined){
-                let opener = OPENERS.indexOf(char);
-                let closer = CLOSERS.indexOf(char);
-                if(opener >= 0 || closer >= 0){
-                    let i = Math.max(opener,closer);
+                if(OPENERS.indexOf(char) !== -1){
+                    let choice = OPENERS.indexOf(char);
                     bracketType = {
-                        opener: OPENERS[i],
-                        closer: CLOSERS[i]
+                        opener: char,
+                        closer: CLOSERS[choice]
                     };
-                    if(closer >= 0){
-                        return new vscode.Position(position.line + line - 1, 999);
+                } else if (CLOSERS.indexOf(char) !== -1){
+                    if(line <= 1){
+                        return new vscode.Position(position.line + line, position.character + index + 1);
                     } else {
-                        if(thisLine && thisLine.length > index && thisLine[index + 1] === bracketType.closer){
-                            // skip open/close | '[]' '{}' '()'
-                            bracketType = undefined;
-                            index++;
-                            continue;
-                        }
-                        count++;
+                        return new vscode.Position(position.line + line - 1, lines[line - 1].length);
                     }
                 }
             } else {
-                if(char === bracketType.opener) {
-                    count++; 
-                }
-                if(char === bracketType.closer) {
-                    if(count <= 1){
+                if(char === bracketType.opener){
+                    count++;
+                } else if (char === bracketType.closer) {
+                    count--;
+                    if(count < 0){
+                        if(line === 0){
+                            return new vscode.Position(position.line + line, position.character + index + 1);
+                        }
                         return new vscode.Position(position.line + line, index + 1);
-                    } else {
-                        count--;
                     }
                 }
-            } 
+            }
         }
     }
-    return new vscode.Position(position.line + MAX_LINES, 0);
+    return new vscode.Position(Math.min(position.line + MAX_LINES, position.line + lines.length - 1), 0);
 }
 
 export function pageUp(doc: vscode.TextDocument, position: vscode.Position): vscode.Position{
@@ -91,14 +90,12 @@ export function pageUp(doc: vscode.TextDocument, position: vscode.Position): vsc
                         closer: CLOSERS[i]
                     };
                     if(opener >= 0){
-                        return new vscode.Position(position.line - line + 1,0);
-                    } else {
-                        if(thisLine && thisLine.length > index && thisLine[thisLine.length - 2 - index] === bracketType.opener){
-                            // skip open/close | '[]' '{}' '()'
-                            bracketType = undefined;
-                            index++;
-                            continue;
+                        if(line <= 1){
+                            return new vscode.Position(position.line - line, thisLine.length - 1 - index);
+                        } else {
+                            return new vscode.Position(position.line - line + 1,0);
                         }
+                    } else {
                         count++;
                     }
                 }
